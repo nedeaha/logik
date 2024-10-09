b47aaee46f285509de5ed4c03adcf18efcbda5b7
Lisa <54662679+abbliseng@users.noreply.github.com>
2024-10-09 12:46:35 +0200

    Updatera u3 till att ta into account konsekutiva

--- lab1/u3.pl
+++ lab1/u3.pl
@@ -1,12 +1,21 @@
+% Genererar alla konesktutiva sublists från F
+partstring(List, L, F) :-
+    con_sublist(F, List),           % Skapa konsekutiva listan.
+    F \= [],                        % Checka så inte 0.
+    length(F, L).                   % Räkna ut längedn av F
+    % L > 0.
 
-%Delar upp listan i subsets
-partstring([H|T], L, F) :-
-    append(F, _, [H|T]),
-    length(F, L), %gGer längden på det nya subset
-    L > 0. %Ett subset kan inte vara tomma listan
+% con_sublist(+Sub, +List)
+% Hjälppredikat för att hitta konsekutiva listor.
+con_sublist(Sub, List) :-
+    append(_, Suffix, List),       % Hitta en suffix av List, alltså ändelse, typ [1,2,3] -> [2,3]
+    append(Sub, _, Suffix),        % Hitta en prefix av Suffix, alltså början, typ [1,2,3] -> [1,2]
+    consecutive(Sub).              % Kolla om Sub är konsekutivt.
 
-%Tar bort listans head och vi får resterande subsets
-partstring([H|T], L, F) :-
-    partstring(T, L, F).
-
-
+% consecutive(+List)
+% Succeeds if all elements of List are consecutive.
+consecutive([]).                   % Empty list is trivially consecutive. 
+consecutive([_]).                  % A single element is trivially consecutive.
+consecutive([X, Y | Rest]) :-      % For two or more elements, ensure they are consecutive
+    Y is X + 1,                    % Ensure X and Y are consecutive
+    consecutive([Y | Rest]).       % Recursively check the rest of the list
