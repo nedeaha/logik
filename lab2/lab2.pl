bc3e3f9741be3e89f2ebb7b6e77ac74ab78409b9
Lisa <54662679+abbliseng@users.noreply.github.com>
2024-11-11 11:31:07 +0100

    Update lab2.pl

--- lab2/lab2.pl
+++ lab2/lab2.pl
@@ -12,45 +12,57 @@ valid_proof(Prems, Goal, Proof, [H|T]) :-
     valid_proof(Prems, Goal, Proof, T),
     apply_rule(H, Prems, Proof).
 
-proof_goal_match(Goal, [_, Goal, M]) :- \+(M==assumption).
+proof_goal_match(Goal, [_, Goal, M]).
 
-apply_rule([H1, H2, H3|_], Prems, Proof) :-
-    (H3==premise, premise(H2, Prems));
-    (H3==assumption, assumption(Proof));
-    (H3=copy(X), H1>X, copy(X, H2, Proof));
-    (H3=andint(X, Y), H1>X, H1>Y, andint(X, Y, H2, Proof));
-    (H3=andel1(X), H1>X, andel1(X, H2, Proof));
-    (H3=andel2(X), H1>X, andel2(X, H2, Proof));
-    (H3=orint1(X), H1>X, orint1(X, H2, Proof));
-    (H3=orint2(X), H1>X, orint2(X, H2, Proof));
-    %(H3=orel(X, Y, U, V, W))
-    (H3=impint(X, Y), H1>X, H1>Y, impint(X, Y, H2, Proof));
-    (H3=impel(X, Y), H1>X, H1>Y, impel(X, Y, H2, Proof));
-    (H3=negint(X, Y), H1>X, H1>Y, negint(X, Y, H2, Proof));
-    (H3=negel(X, Y), H1>X, H1>Y, negel(X, Y, Proof));
-    (H3=contel(X), H1>X, contel(X, Proof));
-    (H3=negnegint(X), H1>X, negnegint(X, H2, Proof));
-    (H3=negnegel(X), H1>X, negnegel(X, H2, Proof));
-    (H3=mt(X, Y), H1>X, H1>Y, mt(X, Y, H2, Proof));
-    (H3=pbc(X, Y), H1>X, H1>Y, pbc(X, Y, H2, Proof));
-    (H3==lem, lem(H2)).
+apply_rule([H1, H2, Line_Rule|_], Prems, Proof) :-
+    (Line_Rule==premise, premise(H2, Prems));
+    (Line_Rule==assumption, assumption(H2, Proof, Prems));
+    (Line_Rule=copy(X), H1>X, copy(X, H2, Proof));
+    (Line_Rule=andint(X, Y), H1>X, H1>Y, andint(X, Y, H2, Proof));
+    (Line_Rule=andel1(X), H1>X, andel1(X, H2, Proof));
+    (Line_Rule=andel2(X), H1>X, andel2(X, H2, Proof));
+    (Line_Rule=orint1(X), H1>X, orint1(X, H2, Proof));
+    (Line_Rule=orint2(X), H1>X, orint2(X, H2, Proof));
+    %(Line_Rule=orel(X, Y, U, V, W))
+    (Line_Rule=impint(X, Y), H1>X, H1>Y, impint(X, Y, H2, Proof));
+    (Line_Rule=impel(X, Y), H1>X, H1>Y, impel(X, Y, H2, Proof));
+    (Line_Rule=negint(X, Y), H1>X, H1>Y, negint(X, Y, H2, Proof));
+    (Line_Rule=negel(X, Y), H1>X, H1>Y, negel(X, Y, Proof));
+    (Line_Rule=contel(X), H1>X, contel(X, Proof));
+    (Line_Rule=negnegint(X), H1>X, negnegint(X, H2, Proof));
+    (Line_Rule=negnegel(X), H1>X, negnegel(X, H2, Proof));
+    (Line_Rule=mt(X, Y), H1>X, H1>Y, mt(X, Y, H2, Proof));
+    (Line_Rule=pbc(X, Y), H1>X, H1>Y, pbc(X, Y, H2, Proof));
+    (Line_Rule==lem, lem(H2)).
 
 %idk om detta funkar
 apply_rule(Box, Prems, Proof) :-
-
+    nth1(1, Box, [Line, P, _]),
+    append_previous_lines(Line, Proof, Box, New_proof),
     last(Box, [_, Goal, _]),
-    valid_proof(Goal, Prems, New_proof, Box).
+    % apply the assumption as a premise
+    append(Prems, [P], New_prems),
+    valid_proof(New_prems, Goal, New_proof, Box).
+    
 
 % Helper function to constuct a proof.
-append_n_elements(0, _, Target, Target). % if n=0, return target
-append_n_elements()
+append_previous_lines(1, _, Box, Box).
+append_previous_lines(Line, Proof, Box, New_proof) :-
+    New_line is Line - 1,
+    nth1(New_line, Proof, Line_to_add),
+    append(Box, [Line_to_add], New_box),
+    append_previous_lines(New_line, Proof, New_box, New_proof).
+
 
 %Alla regler
 premise(H2, Prems) :- 
     member(H2, Prems).
 
-assumption(Proof) :-
-    nth1(1, Proof, [_, _, assumption]).
+assumption(H2, Proof, Prems) :-
+    nth1(1, Proof, [_, _, assumption]),
+    % make sure the assumption starts a box
+    write(H2),
+    member(H2, Prems).
 
 copy(X, P, Proof) :- 
     member([X, P, _], Proof).
@@ -71,7 +83,7 @@ orint1(X, or(P, _), Proof) :-
 orint2(X, or(_, Q), Proof) :- 
     member([X, Q, _], Proof). 
 
-%orel(X, Y, U, V, W) :-
+% orel(X, Y, U, V, W) :-
 
 %kanske inte klar
 impint(X, Y, imp(P, Q), Proof) :- 
